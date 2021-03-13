4dd8ecdda662fa73f27a8c73b7cb57a8c989a7e1
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-13 23:52:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 function add() {
-
+  
 }
 
 function subtract() {
-
+  
 }
 
 function multiply() {
-
+  
 }
 
 function divide() {
-
+  
 }
+
+var number = 10
+ 
+function add5() {
+  number += 5
+}
+ 
+function divideBy3() {
+  number /= 3
+}
+ 
+divideBy3()
+ 
+console.log(number) // 3.333333333335
+ 
+add5()
+ 
+console.log(number) // 8.333333333335
+ 
+// reset number
+number = 10
+ 
+add5()
+ 
+console.log(number) // 15
+ 
+divideBy3()
+ 
+console.log(number) // 5
