ec40f287d52cd4341b6f83684acb779f97db036e
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-14 00:41:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,10 +30,9 @@ function parseFloat() {
 
 }
 function makeInt(n) {
-n = parseInt(n, 10);
-return n;
+return parseInt(n, 10);
 }
 
 function preserveDecimal(n) {
-return parseFloat(n);
+return parseFloat(n, 10);
 }
