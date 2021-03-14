d7461cc46fb82456f2da737372eefff19cc10ee0
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-14 00:21:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function add(a, b) {
 }
 
 function subtract(a, b) {
-
+return a - b;
 }
 
 function multiply(a, b) {
-
+return a * b;
 }
 
 function divide(a, b) {
-
+return a / b;
 }
 
 function parseInt() {
