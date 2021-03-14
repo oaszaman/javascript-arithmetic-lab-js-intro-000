de43d90e25b06a239c2568fd622348be8821a780
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-14 00:10:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function multiply(a, b) {
 function divide(a, b) {
 
 }
+
+function parseInt() {
+  
+}
+
+function parseFloat() {
+
+}
+function makeInt(string) {
+  
+}
+
+function preserveDecimal(string) {
+  
+}
